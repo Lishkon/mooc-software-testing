4892e8c03fdfaf65e1bd93a0e661c44ebfaf742e
lishkon <lishkon@gmail.com>
2019-01-31 02:16:22 +0200

    Numfinder is fixed

--- src/main/java/tudelft/numfinder/NumFinder.java
+++ src/main/java/tudelft/numfinder/NumFinder.java
@@ -1,17 +1,18 @@
 package tudelft.numfinder;
 
+import java.util.Arrays;
+
 public class NumFinder {
     private int smallest = Integer.MAX_VALUE;
     private int largest = Integer.MIN_VALUE;
 
     public void find(int[] nums) {
-        for(int n : nums) {
-
-            if(n < smallest)
-                smallest = n;
-            else if (n > largest)
-                largest = n;
-
+        // Let's sort the array first in ascending order
+        Arrays.sort(nums);
+        // Finding the smallest and largest elements
+        if (nums[0] < nums[nums.length-1]) {
+            smallest = nums[0];
+            largest = nums[nums.length - 1];
         }
     }
 
